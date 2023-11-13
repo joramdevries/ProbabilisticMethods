586c406e191c011e88c7e1f9fad514ddc52a9a78
joramdevries <joramdevries@live.nl>
2023-11-13 14:32:41 +0100

    Creation of Final Project

---
+++ Final_Project/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 13 14:29:54 2023
+
+@author: joram
+"""
+#%% IMPORT LIBRARIES
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+import os
+
+cur = os.getcwd()
+
