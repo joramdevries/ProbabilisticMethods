910b0ec62f0e09cfd6844a5ea60ee6f954a023f6
joramdevries <joramdevries@live.nl>
2023-11-13 14:35:34 +0100

    created main.py

--- Final_Project/main.py
+++ Final_Project/main.py
@@ -4,12 +4,41 @@ Created on Mon Nov 13 14:29:54 2023
 
 @author: joram
 """
-#%% IMPORT LIBRARIES
-import matplotlib.pyplot as plt
+#%% IMPORTING LIBRARIES
+
 import numpy as np
 import pandas as pd
+import scipy.stats as stats
+import scipy
+import matplotlib.pyplot as plt
+import sklearn
+import sklearn.neural_network as nn
 
 import os
 
+import openturns as ot
+
+from scipy.stats import truncnorm, qmc
+
+from datetime import datetime, timedelta
+
+import warnings
+
+
+# %% CUR
 cur = os.getcwd()
 
+# %% FILTER WARNINGS
+
+# Suppress all warnings
+warnings.filterwarnings("ignore")
+
+# %% IMPORT DATA
+
+LiDAR_Data = pd.read_csv('lidar_data_2hz.csv')
+
+LiDAR_Data['Timestamp'] = pd.to_datetime(LiDAR_Data['Timestamp'], format='%Y%m%d%H%M')
+
+LiDAR_Data['Year'] = LiDAR_Data['Timestamp'].dt.year
+
+print("Original Data has length of ", len(LiDAR_Data))
