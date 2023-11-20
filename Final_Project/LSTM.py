ef73ce46a4ff370695e1f0695f4a265f058c7ddd
joramdevries <joramdevries@live.nl>
2023-11-20 16:15:59 +0100

    Push Push Baby

--- Final_Project/LSTM.py
+++ Final_Project/LSTM.py
@@ -29,6 +29,9 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 import tensorflow.keras.backend as K
+from tensorflow.keras.layers import Activation
+
+from tensorflow.keras.layers import LSTM, Masking
 
 # Use Tensorboard for network visualization & debugging
 from tensorflow.keras.callbacks import TensorBoard
@@ -73,9 +76,20 @@ def data_import():
     
     print(data.tail)
     
+    # Calculate the rolling mean for the last 10 minutes (600 seconds)
+    rolling_mean = data['Wsp_44m'].rolling(window=1200).mean()
+    
+    # Calculate the rolling standard deviation for the last 10 minutes (600 seconds)
+    rolling_std = data['Wsp_44m'].rolling(window=1200).std()
+    
+    # Create a new column 'Mean Ws' with the calculated rolling mean values
+    data['Mean WS'] = rolling_mean
+    data['STD'] = rolling_std
+    data['TI'] = rolling_std/rolling_mean
+        
     return data
 
-def LSTM(data, output):
+def FFNN(data, output):
 
     print('Null values: ', data.isnull().sum())
     
@@ -168,14 +182,14 @@ def LSTM(data, output):
 
     # Adjust the learning rate as needed
     custom_optimizer = Adam(learning_rate=0.001)
-    model.compile(loss='mean_squared_error', optimizer=custom_optimizer, metrics=['binary_accuracy'])
+    model.compile(loss='mean_squared_error', optimizer=custom_optimizer)
     
     lr_scheduler = LearningRateScheduler(lr_schedule)
     early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
     
     # Add batch normalization and dropout layers as needed
     model.add(BatchNormalization())
-    model.add(Dropout(0.25))
+    model.add(Dropout(0.125))
     
     #model.compile(loss='mean_squared_error', optimizer='adam')
     #model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['binary_accuracy'])
@@ -201,10 +215,10 @@ def LSTM(data, output):
         plt.legend()
         plt.show()
         
-        plt.plot(history.history['binary_accuracy'], label='train_accuracy')
-        plt.plot(history.history['val_binary_accuracy'], label='validation_accuracy')
-        plt.legend()
-        plt.show()
+        #plt.plot(history.history['binary_accuracy'], label='train_accuracy')
+        #plt.plot(history.history['val_binary_accuracy'], label='validation_accuracy')
+        #plt.legend()
+        #plt.show()
     
     # calculate predictions for validation dataset
     pred_val = model.predict(X_validation_scaled)
@@ -227,10 +241,289 @@ def LSTM(data, output):
     #plt.plot(rounded_pred_test,'.', label = 'rounded predictions')
     plt.legend()
     plt.show()
+    
+    
+def LSTM_function(data, output):
+    
+        ### define a function that will prepare the shifting input sequences for the network
+    def forecast_sequences_input(input_data,n_lag):
+        """
+        A function that will split the input time series to sequences for nowcast/forecast problems
+        Arguments:
+            input_data: Time series of input observations as a list, NumPy array or pandas series
+            n_lag: number of previous time steps to use for training, a.k.a. time-lag        
+        Returns:
+            Pandas DataFrame of series framed for supervised learning.
+        """
+        n_vars = input_data.shape[1] 
+        df = pd.DataFrame(input_data)
+        cols, names = list(), list()
+        # input sequence (t-n, ... t-1)
+        for i in range(n_lag, 0, -1):
+            cols.append(df.shift(i))
+            names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
+        # put it all together (aggregate)
+        agg = pd.concat(cols, axis=1)
+        agg.columns = names
+        return agg
+
+
+    ### define a function that will prepare the shifting output sequences of the network
+    def forecast_sequences_output(output_data,n_out):
+        """
+        A function that will split the output time series to sequences for nowcast/forecast problems
+        Arguments:
+            output_data: Time series of input observations as a list, NumPy array or pandas series
+            n_out: forecast horizon (for multi-output forecast)
+        Returns:
+            Pandas DataFrame of series framed for supervised learning.
+        """
+        n_vars = output_data.shape[1] 
+        df = pd.DataFrame(output_data)
+        cols, names = list(), list()
+        # forecast sequence (t, t+1, ... t+n)
+        for i in range(0, n_out):
+            cols.append(df.shift(-i))
+            if i == 0:
+                names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
+            else:
+                names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
+        # put it all together (aggregate)
+        agg = pd.concat(cols, axis=1)
+        agg.columns = names    
+        return agg
+    
+    n_lag = 6 # number of previous time steps to use for training, a.k.a. time-lag
+    n_out = 1  # forecast horizon [s] 
+    
+    ### Split data into train & test 
+    
+    input1 = 'Wsp_44m'
+    input2 = 'Wdir_41m'
+
+    train_int = int(0.6*len(data)) # 60% of the data length for training
+    validation_int = int(0.8*len(data)) # 20% more for validation
+    
+    # training input vector
+    X_train = data[[input1, input2]][:train_int]
+    X_train = forecast_sequences_input(X_train,n_lag)
+    
+    # training output vector
+    Y_train = data[[output]][:train_int]
+    Y_train = forecast_sequences_output(Y_train, n_out)
+    
+    # validation input vector
+    X_validation = data[[input1, input2]][train_int:validation_int]
+    X_validation = forecast_sequences_input(X_validation,n_lag)
+    
+    # validation output vector
+    Y_validation = data[[output]][train_int:validation_int]
+    Y_validation = forecast_sequences_output(Y_validation, n_out)
+    
+    # test input vector
+    X_test = data[[input1, input2]][validation_int:]
+    X_test = forecast_sequences_input(X_test,n_lag)
+    
+    # test output vector
+    Y_test = data[[output]][validation_int:]
+    Y_test = forecast_sequences_output(Y_test, n_out)
+    
+    ### scale the dataset
+    scaler = MinMaxScaler(feature_range=(-1, 1))
+    
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_validation_scaled = scaler.transform(X_validation)
+    X_test_scaled = scaler.transform(X_test)
+    
+    print('Training input (samples, timesteps):', X_train_scaled.shape)
+    print('Training output (samples, timesteps):', Y_train.shape)
+    print('Validation input (samples, timesteps):', X_validation_scaled.shape)
+    print('Validation output (samples, timesteps):', Y_validation.shape)
+    
+    
+    pad_value = 999
+
+    X_train_scaled[np.isnan(X_train_scaled)] = pad_value
+    X_validation_scaled[np.isnan(X_validation_scaled)] = pad_value
+    X_test_scaled[np.isnan(X_test_scaled)] = pad_value
+    
+    # for multiple model creation - clear  the previous DAG
+    K.clear_session() 
+    
+    ### Input reshape for LSTM problem  [samples, timesteps, features]
+    no_features = 2 # Avg and Std of wind speed
+    
+    train_X = X_train_scaled.reshape((X_train_scaled.shape[0], n_lag, no_features))#.astype('float32')
+    train_Y = Y_train.values#.astype('float32')
+    
+    validation_X = X_validation_scaled.reshape((X_validation_scaled.shape[0], n_lag, 2))#.astype('float32')
+    validation_Y = Y_validation.values#.astype('float32')
+    
+    test_X = X_test_scaled.reshape((X_test_scaled.shape[0], n_lag, 2))#.astype('float32')
+    test_Y = Y_test.values#.astype('float32')
+    
+    ### create model
+    model = Sequential()
+    
+    # Masking layer (for the pad_value)
+    model.add(Masking(mask_value=pad_value, input_shape=(None, no_features)))
+    
+    # First LSTM layer
+    model.add(LSTM(50, 
+                   return_sequences=True,  # important to add it to ensure the following LSTM layers will have the same input shape
+                   input_shape=(train_X.shape[1], train_X.shape[2]),                
+                   kernel_initializer='random_uniform',
+                   bias_initializer='zeros'))
+                   
+    # then we add the activation
+    model.add(Activation('relu'))
+    
+    # Second LSTM layer
+    model.add(LSTM(10, activation='relu'))
+    
+    # Output Layer
+    model.add(Dense(1, activation='linear'))
+    model.summary()
+    
+    # compile the model
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    
+    # fit the model and store the graphs and performance to be used in TensorBoard (optional)
+    now = datetime.now().strftime("%Y%m%d_%H%M")
+    
+    tbGraph = TensorBoard(log_dir=f'.\Graph\{now}',
+                          histogram_freq=64*2, write_graph=True, write_images=True)
+    
+    history = model.fit(train_X, train_Y, 
+              epochs=30,
+              batch_size=64,
+              verbose=2,
+              validation_data=(validation_X, validation_Y),
+              callbacks=[tbGraph])
+    
+    ### plot history
+    plt.plot(history.history['loss'], label='train')
+    plt.plot(history.history['val_loss'], label='validation')
+    plt.legend()
+    plt.show()
+    
+    plt.plot(history.history['binary_accuracy'], label='train_accuracy')
+    plt.plot(history.history['val_binary_accuracy'], label='validation_accuracy')
+    plt.legend()
+    plt.show()
+    
+    
+def understanding(df, output):
+    
+    start_timestamp = pd.to_datetime('07/09/2020 13:30:00', format="%d/%m/%Y %H:%M:%S")
+    end_timestamp = pd.to_datetime('07/09/2020 13:30:20', format="%d/%m/%Y %H:%M:%S")
+    
+    filtered = df[(df.index > start_timestamp) & (df.index < end_timestamp)]
+    
+    print(filtered)
+    
+    # Create subplots in a single row
+    fig, axs = plt.subplots(4, 1, figsize=(15, 8))
+    
+    # Plot on the first subplot
+    axs[0].plot(filtered.index, filtered['ActPow'])
+    axs[0].set_ylabel(r'$P [kW]$')
+    axs[0].grid(True)
+    
+    
+    # Plot on the second subplot
+    axs[1].plot(filtered.index, filtered['MxA1_auto'], 'blue', label = "Blade A")
+    axs[1].plot(filtered.index, filtered['MxB1_auto'], 'orange', label = "Blade B")
+    axs[1].plot(filtered.index, filtered['MxC1_auto'], 'green', label = "Blade C")
+    axs[1].set_ylabel(r'$M_{bf} [kNm]$')
+    axs[1].legend()
+    axs[1].grid(True)
+    
+    # Plot on the third subplot
+    axs[2].plot(filtered.index, filtered['W2_Vlos1_orig'] , 'blue', label = "Vlos1")
+    axs[2].plot(filtered.index, filtered['W2_Vlos2_orig'] , 'orange', label = "Vlos2")
+    axs[2].set_ylabel(r'$U [m/s]$')
+    axs[2].legend()
+    axs[2].grid(True)
+    
+    # Plot on the fourth subplot
+    axs[3].plot(filtered.index, filtered['W4_Vlos1_orig'] , 'blue', label = "Vlos1")
+    axs[3].plot(filtered.index, filtered['W4_Vlos2_orig'] , 'orange', label = "Vlos2")
+    axs[3].plot(filtered.index, filtered['W4_Vlos3_orig'] , 'green', label = "Vlos3")
+    axs[3].plot(filtered.index, filtered['W4_Vlos4_orig'] , 'red', label = "Vlos4")
+    axs[3].set_xlabel('Time')
+    axs[3].set_ylabel(r'$U [m/s]$')
+    axs[3].legend()
+    axs[3].grid(True)
+    
+    # Add legends after all lines have been plotted
+    #axs[1].legend(loc='bottom right')
+    #axs[2].legend(loc='bottom right')
+    #axs[3].legend(loc='bottom right')
+    
+    # Adjust layout for better appearance
+    plt.tight_layout()
+    
+    # Show the plots
+    plt.show()
+    
+    
+    print('lenght of df =',len(df))
+    df = df[df['Mean WS']>4]
+    print('lenght of df =',len(df))
+    #df = data[265<data['Wdir_41m']<295]
+    print('lenght of df =',len(df))
+    data = df[0<df['ActPow']]
+    print('lenght of df =',len(data))
+    df = data[16<data['ROT']]
+    print('lenght of df =',len(df))
+    data = df[df['Pitch']<23]
+    print('lenght of df =',len(data))
+    
+    df = data.dropna()
+    
+    wind_speed_data = df['Mean WS']
+    ti_data = df['TI']
+    
+    # Create a 2D histogram
+    hist, x_edges, y_edges = np.histogram2d(wind_speed_data, ti_data, bins=(20, 20))
+    
+    # Create a meshgrid for the heatmap
+    x, y = np.meshgrid(x_edges[:-1], y_edges[:-1])
+    
+    # Plot the 2D histogram as a heatmap
+    plt.figure(figsize=(10, 8))
+    plt.pcolormesh(x, y, hist.T, cmap='viridis')
+    plt.colorbar(label='Count')
+    
+    # Add labels and title
+    plt.xlabel('Wind Speed (m/s)')
+    plt.ylabel('Turbulence Intensity')
+    plt.title('Wind Speed vs Turbulence Intensity Distribution')
+    
+    # Show the plot
+    plt.grid(True)
+    plt.show()
+    
 
 
 # %% MAIN
 if __name__ == '__main__':
+    
+    # %% CONTROL BOARD
+    
+    # Select model
+    show_FFNN = False
+    show_LSTM = True
+    show_understanding = False
+    
+    #%% MAIN LOOP
+
     data = data_import()
     output = 'MxA1_auto'
-    LSTM(data, output)
+    if show_FFNN:
+        FFNN(data, output)
+    if show_LSTM:
+        LSTM_function(data, output)
+    if show_understanding:
+        understanding(data, output)
